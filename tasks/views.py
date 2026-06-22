57d5acbc346763545a38b17fae4091d9adde55aa
Dostonbek <dostonbeksuyunov85@gmail.com>
2026-06-22 14:55:54 +0500

    drf-1: DRF setup va birinchi API

--- tasks/views.py
+++ tasks/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def salom(request):
-    return JsonResponse({'message':'Hello World!'})
+    return JsonResponse({'message':'Hello DRF!'})
 
